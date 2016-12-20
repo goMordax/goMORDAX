406c3efe8392e4cd4fcb795cc447ec54cd4251fc
toni <abailon@BAILON-PC>
2016-12-20 23:45:45 +0100

    Quitar referencia que no se usaba

--- Web1.Tests/Controllers/ValuesControllerTest.cs
+++ Web1.Tests/Controllers/ValuesControllerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Web1;
 using Web1.Controllers;
